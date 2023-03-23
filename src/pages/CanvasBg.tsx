633e96e8188050b28aeec3f2b4f3ff10b4c58cac
SanPhantom <1300690777@qq.com>
2023-03-23 10:59:58 +0800

    fix: update

--- src/pages/CanvasBg.tsx
+++ src/pages/CanvasBg.tsx
@@ -1,65 +1,74 @@
 import { Box } from "@mui/material";
-import { useMemoizedFn, useSize } from "ahooks";
+import { useInterval, useMemoizedFn, useSize } from "ahooks";
 import React, { useRef } from "react";
 import { Layer, Stage } from "react-konva";
 import {
-  Spring,
   animated,
+  config,
+  SpringValue,
   useSpring,
   useTransition,
 } from "@react-spring/konva";
 import { HitContext } from "konva/lib/Context";
 import { Shape, ShapeConfig } from "konva/lib/Shape";
 
-const Particle = ({ num = 50 }: { num?: number }) => {
-  const data = new Array(num).fill(0);
-
-  const randomColor = useMemoizedFn(() => {
-    const randomHex = () => {
-      return Math.floor(Math.random() * 255);
-    };
-    const alpha = () => (Math.floor(Math.random() * 10) + 1) / 10 / 2;
-    return `rgba(${randomHex()}, ${randomHex()}, ${randomHex()}, ${alpha()})`;
+const Particle = (props: { index: number }) => {
+  const randomHex = useMemoizedFn(() => {
+    return Math.floor(Math.random() * 255);
   });
-
   const randomNum = useMemoizedFn((max: number) => {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max) + 1;
   });
 
-  const springProps = useTransition(data, {
-    key: (_: any, index: number) => `particle_${index}`,
-    form: () => ({ opacity: 0 }),
-    enter: () => ({ opacity: 1 }),
-    leave: () => ({ opacity: 0 }),
-    loop: true,
-  });
+  const red = randomHex();
+  const green = randomHex();
+  const blue = randomHex();
+
+  const x = randomNum(1100);
+  const y = randomNum(1100);
+  const r = 2;
+
+  const [springProps, api] = useSpring(
+    () => ({
+      form: { fill: `rgba(${red}, ${green}, ${blue}, 0)` },
+      to: [
+        { fill: `rgba(${red}, ${green}, ${blue}, 0.25)` },
+        { fill: `rgba(${red}, ${green}, ${blue}, 0.5)` },
+        { fill: `rgba(${red}, ${green}, ${blue}, 0.75)` },
+        { fill: `rgba(${red}, ${green}, ${blue}, 1)` },
+      ],
+      config: config.wobbly,
+      loop: {
+        reverse: true,
+        delay: props.index,
+      },
+    }),
+    []
+  );
+
+  return (
+    // @ts-ignore
+    <animated.Shape
+      {...springProps}
+      sceneFunc={(context: HitContext, shape: Shape) => {
+        const { width, height } = context.canvas;
+        context.beginPath();
+        context.arc(x, y, r, 0, 2 * Math.PI, false);
+        context.closePath();
+        context.fillStrokeShape(shape);
+      }}
+    />
+  );
+};
+
+const ParticleCanvas = ({ num = 50 }: { num?: number }) => {
+  const data = new Array(num).fill(0);
 
   return (
     <>
-      {data.map((_, index) => (
-        // @ts-ignore
-        <animated.Shape
-          key={`particle_${index}`}
-          {...springProps}
-          fill={randomColor()}
-          sceneFunc={(context: HitContext, shape: Shape) => {
-            const { width, height } = context.canvas;
-            // context.fillStyle = randomColor();
-            context.beginPath();
-            context.arc(
-              randomNum(width),
-              randomNum(height),
-              randomNum(3),
-              0,
-              2 * Math.PI,
-              false
-            );
-            context.closePath();
-            context.fill();
-            context.fillStrokeShape(shape);
-          }}
-        />
-      ))}
+      {data.map((item, index) => {
+        return <Particle key={`particle_${index}`} index={index} />;
+      })}
     </>
   );
 };
@@ -72,7 +81,7 @@ const CanvasBg = () => {
     <Box sx={{ width: "100%", height: "100%" }} ref={containerRef}>
       <Stage {...size}>
         <Layer>
-          <Particle num={2250} />
+          <ParticleCanvas num={120} />
         </Layer>
       </Stage>
     </Box>
